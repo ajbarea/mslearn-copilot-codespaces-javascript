0946fae938729470a0bf7ad90434617fa2413986
AJ Barea <79944464+ajbarea@users.noreply.github.com>
2024-06-04 20:09:49 +0000

    chore: Update npm dependency to latest version of caniuse-lite - Update Portfolio App details - Add bounce animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "AJ Barea",
+  title: "Computer Engineer & Software Developer",
+  email: "ajbareaa@gmail.com",
+  gitHub: "ajbarea",
+  instagram: "",
+  linkedIn: "aj-barea",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "ajbarea",
 };
 
 const primaryColor = "#4E567E";
